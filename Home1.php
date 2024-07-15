6e8eb2d73422f5fb5aa70b6e9786cb7fca73243c
Mohammad-siyam <mds977064@gmail.com>
2024-07-16 02:30:40 +0530

    Added Home1.php

---
+++ Home1.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0  shink-to-fit=no">
+  <link rel="stylesheet" href="css/css/bootstrap.css">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"></script>
+  <title>Doctor Appointment Management System </title>
+</head>
+
+<body>
+
+  <div class="container mb-5">
+
+    <div class="row py-3">
+      <div class="col">
+
+      <?php include("nav1.php");?>
+
+        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-lg">
+          <a href="Home.php" class="Navbar-brand"> Home </a>
+          <ul class="navbar-nav">
+
+            <li class="nav-item"> <a href="about.php" class="nav-link"> About </a></li>
+            <li class="nav-item"> <a href="checkappoi.php" class="nav-link">Check Appointments</a></li>
+            <li class="nav-item"> <a href="booking.php" class="nav-link">Booking</a></li>
+            <li class="nav-item"> <a href="contact.php" class="nav-link">Contact</a></li>
+            <li class="nav-item"> <a href="login.php" class="nav-link">Doctor</a></li>
+          </ul>
+
+        </nav> -->
+      </div>
+    </div>
+    <h2 class="py-3 text-center"> Doctor Appointment Management System </h2>
+    <div class="carousel-item active">
+      <img src="images/slider/doctor.jpg" alt="image could not found " class="img-fluid">
+    </div>
+    <div class="carousel-item ">
+      <img src="images/slider/assistant.jpg" alt="image could not found " class="img-fluid">
+    </div>
+    <div class="carousel-item ">
+      <img src="images/slider/hand.jpg" alt="image could not found " class="img-fluid">
+    </div>
+  </div>
+
+
+
+
+
+</body>
+
+</html>
