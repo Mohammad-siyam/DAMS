5330222e9ec080606f044f03ecc8e140f7e9c78f
Mohammad-siyam <mds977064@gmail.com>
2024-07-16 02:36:56 +0530

    about is uploaded

---
+++ about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
+    <link rel="stylesheet" href="css/css/bootstrap.css">
+    <link rel="stylesheet" href="about.css">
+    <title>About Us </title>
+</head>
+
+<body>
+       
+    <div class="row py-3">
+        <div class="col">
+        <?php include("nav1.php");?>
+
+            <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-lg">
+                <a href="Home.php" class="Navbar-brand"> Home </a>
+                <ul class="navbar-nav">
+
+                    <li class="nav-item"> <a href="about.php" class="nav-link"> About </a></li>
+                    <li class="nav-item"> <a href="checkappoi.php" class="nav-link">Check Appointments</a></li>
+                    <li class="nav-item"> <a href="booking.php" class="nav-link">Booking</a></li>
+                    <li class="nav-item"> <a href="contact.php" class="nav-link">Contact</a></li>
+                    <li class="nav-item"> <a href="login.php" class="nav-link">Doctor</a></li>
+                </ul>
+
+            </nav> -->
+        </div>
+    <div class="container1">
+           <h1> About Us </h1>
+        <div class="container2">
+            <h1> Our Vision </h1>
+        </div>
+        <div class="container3">
+            <h1> Our Mission </h1>
+        </div>
+
+    </div>
+</body>
+
+</html>
